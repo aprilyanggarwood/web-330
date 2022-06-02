f8b4bb2feea8158a117779d9cad0cfb67b51108c
aprilyanggarwood <yangyyj822@hotmail.com>
2022-06-01 19:01:57 -0700

    initial added week-2 html and css

---
+++ week-2/palindrome.js
@@ -0,0 +1,12 @@
+/**
+    Title: palindrome.js
+    Author: April Yang
+    Date: 06/01/2022
+    Description: Javascript File for palindrome.html
+//
+ */
+
+function getTodaysDate() {
+  let d = new Date(en - US);
+  let today = d.getDate(en - US);
+}
