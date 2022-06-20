968946dea436707fdd5449b5c79935ea970ef143
aprilyanggarwood <yangyyj822@hotmail.com>
2022-06-20 02:02:41 -0700

    edited comments

--- week-4/calorie-converter.js
+++ week-4/calorie-converter.js
@@ -1,6 +1,16 @@
+/*
+  Title: calorie-converter.js
+  Author: April Yang
+  Date: 06/18/2022
+  Description: Creates CalorieConverter class and export module.
+*/
+
+// Import FoodModel class.
 import { FoodModel } from "./food-model.js";
 
+// Created CalorieConverter class.
 export class CalorieConverter {
+  // Creates an array for each food object with the static property name data.
   static data = [
     new FoodModel(1007, "Egg", 78),
     new FoodModel(1008, "Apple", 95),
@@ -10,6 +20,7 @@ export class CalorieConverter {
     new FoodModel(1012, "Soda", 150),
   ];
 
+  // Use static property find method to return an array of objects if the object.name is included in user input.
   static find(string) {
     return this.data.filter((foodModel) => {
       if (string.includes(foodModel.name.toLowerCase())) {
--- week-4/food-model.js
+++ week-4/food-model.js
@@ -1,4 +1,12 @@
+/*
+  Title: food-model.js
+  Author: April Yang
+  Date: 06/18/2022
+  Description: Creates FoodModel class and export module.
+*/
+
 export class FoodModel {
+  // Set up three parameters:id, name, and calories in constructor
   constructor(id, name, calories) {
     this.id = id;
     this.name = name;
