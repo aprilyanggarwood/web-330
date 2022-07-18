dfc6926f86124a366b44cc0c2609eaf637b91aa4
aprilyanggarwood <yangyyj822@hotmail.com>
2022-07-18 01:52:30 -0700

    edited comments

--- week-8/http-client.js
+++ week-8/http-client.js
@@ -1,10 +1,20 @@
+/* Title: Assignment 8.2 - WhatABook, Part 2
+  Author: April Yang
+  Date: 07/16/2022
+Description: Create and export HttpClient class
+  */
+
+// Create HttpClient class.
 export class HttpClient {
   async get(url, params = "") {
+    // Creates New URL object.
     url = new URL(url);
+    // Assign url.search property to URLSearchParams object.
     url.search = new URLSearchParams(params);
     const res = await fetch(url.toString(), {
       method: "GET",
     });
+    // Return res object as JSON.
     return res.json();
   }
 }
